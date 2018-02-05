c4e4505017edba62d6ba85461d247334295590a4
milosz <miloszloszek@gmail.com>
2018-02-05 20:43:13 +0100

    �:worried:

---
+++ Assets/JumpControllerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class JumpControllerScript : MonoBehaviour {
+
+    public FirstPersonController controller;
+    public Rigidbody rdgbd;
+
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            SwitchGravity();
+        }
+    }
+
+    void SwitchGravity()
+    {
+        controller.m_GravityMultiplier = controller.m_GravityMultiplier == 0f ? 2f : 0;
+        rdgbd.useGravity = rdgbd.useGravity == true ? false : true;
+    }
+
+}
