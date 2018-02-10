7d33397dfc88ff2cdd1bb7b3452d7e44e00339f5
milosz <miloszloszek@gmail.com>
2018-02-10 16:47:47 +0100

    materials and light

---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SpawnerScript : MonoBehaviour {
+
+    public Canvas FadeOutScreen;
+    public GameObject RespawnPoint;
+
+    private CanvasGroup group;
+    private Vector3 initialPosition;
+
+    void OnEnable()
+    {
+        group = FadeOutScreen.GetComponent<CanvasGroup>();
+        initialPosition = RespawnPoint.transform.position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(FadeOut(other.gameObject));
+        }
+    }
+
+   IEnumerator FadeOut(GameObject player)
+    {
+        while (group.alpha < 1)
+        {
+            group.alpha += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+        }
+        
+        player.transform.position = initialPosition;
+        group.alpha = 0;
+    }
+}
