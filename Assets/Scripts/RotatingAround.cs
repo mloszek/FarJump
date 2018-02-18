1ff1ed8d037ebda7388ccbf756dcdb99ef70e6d9
milosz <miloszloszek@gmail.com>
2018-02-18 15:42:14 +0100

    level 4

---
+++ Assets/Scripts/RotatingAround.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingAround : MonoBehaviour {
+
+    public GameObject centerObject;
+    [SerializeField] bool clockwise;
+    public int speed;
+
+    private Vector3 orientation;
+
+    void Start()
+    {
+        if (clockwise)
+        {
+            orientation = Vector3.up;
+        }
+        else
+        {
+            orientation = Vector3.down;
+        }
+    }
+
+	void Update () {
+        transform.RotateAround(centerObject.transform.position, orientation, speed * Time.deltaTime);
+	}
+}
