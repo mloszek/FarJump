41b24fd57e50aca7b0ec7c90af2b1dd2daf0d19e
milosz <miloszloszek@gmail.com>
2018-02-24 15:27:38 +0100

    exit menu

---
+++ Assets/Scripts/ExitMenuScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ExitMenuScript : MonoBehaviour {
+
+    public Text text;
+
+    private bool isTextShown = false;
+
+    void Start()
+    {
+        text.gameObject.SetActive(false);
+    }
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            text.gameObject.SetActive(text.gameObject.activeInHierarchy == false ? true : false);
+        }
+
+        if (Input.GetKeyDown("n"))
+        {
+            if (text.gameObject.activeInHierarchy == true)
+            {
+                text.gameObject.SetActive(false);
+            }
+        }
+
+        if (Input.GetKeyDown("y"))
+        {
+            if (text.gameObject.activeInHierarchy == true)
+            {
+                SceneManager.LoadScene("titleScene");
+            }
+        }
+    }
+}
